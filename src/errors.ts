c3871a7897f60448d87bc68c2ff1f8a38f335364
Jonah Snider <jonah@jonahsnider.com>
2024-07-01 21:55:55 -0700

    feat: support multiple handlers for a single subscription

--- src/errors.ts
+++ src/errors.ts
@@ -9,12 +9,14 @@
 
 import { createError } from '@poppinss/utils'
 
+/** @deprecated */
 export const E_MULTIPLE_REDIS_SUBSCRIPTIONS = createError<[string]>(
   'Cannot subscribe to "%s" channel. Channel already has an active subscription',
   'E_MULTIPLE_REDIS_SUBSCRIPTIONS',
   500
 )
 
+/** @deprecated */
 export const E_MULTIPLE_REDIS_PSUBSCRIPTIONS = createError<[string]>(
   'Cannot subscribe to "%s" pattern. Pattern already has an active subscription',
   'E_MULTIPLE_REDIS_PSUBSCRIPTIONS',
