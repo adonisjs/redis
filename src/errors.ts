56192256ae614941b931d140c76380d52fbdfbe4
Harminder Virk <virk.officials@gmail.com>
2025-09-08 15:22:51 +0530

    feat: remove deprecated errors
    BREAKING CHANGE: Error have been removed

--- src/errors.ts
+++ src/errors.ts
@@ -9,20 +9,6 @@
 
 import { createError } from '@poppinss/utils/exception'
 
-/** @deprecated */
-export const E_MULTIPLE_REDIS_SUBSCRIPTIONS = createError<[string]>(
-  'Cannot subscribe to "%s" channel. Channel already has an active subscription',
-  'E_MULTIPLE_REDIS_SUBSCRIPTIONS',
-  500
-)
-
-/** @deprecated */
-export const E_MULTIPLE_REDIS_PSUBSCRIPTIONS = createError<[string]>(
-  'Cannot subscribe to "%s" pattern. Pattern already has an active subscription',
-  'E_MULTIPLE_REDIS_PSUBSCRIPTIONS',
-  500
-)
-
 export const E_INVALID_BYTES_VALUE = createError<[string | number]>(
   'Invalid bytes value "%s"',
   'E_INVALID_BYTES_VALUE',
